2afb0d0df895297ace1454a91d78dea3b264c651
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2021-10-11 02:31:44 +0530

    Week3 Codes Started

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, response
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. 2107931d is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def detail(request,question_id):
+    return HttpResponse("You are Looking at Question %s" % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the result of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
