f2ea5b1812613716d99455c71fe6d7cd49e7255d
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2021-10-16 20:23:18 +0530

    Adding Latest Codes for Results and Details View

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, response,Http404
-from .models import Question
+from django.http import HttpResponse, response,Http404, HttpResponseRedirect
+from .models import Question,Choice
 from django.template import loader
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -28,11 +29,28 @@ def detail(request,question_id):
 
 
 def results(request, question_id):
-    response = "You are looking at the result of question %s"
-    return HttpResponse(response % question_id)
+    # response = "You are looking at the result of question %s"
+    # return HttpResponse(response % question_id)
+
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
-    return HttpResponse("You are voting on question %s." % question_id)
+    #return HttpResponse("You are voting on question %s." % question_id)
+
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'polls/detail.html' , {
+            'question' : question_id,
+            'error_message' : "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) #To Prevent Data from being posted twice
+    
 
 def owner(request):
     return HttpResponse("Hello, World 816a6db4 is the polls index")
