a5f3adda78819b3fcd6cd6fd37a7c4551adea021
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2021-11-09 13:48:22 +0530

    Updating Changes to Assignment

--- hello/views.py
+++ hello/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myview(request):
-    resp = HttpResponse()
+    resp = HttpResponse("In a view - the dj4e cookie value is ad7ed5d4")
     resp.set_cookie('dj4e_cookie','ad7ed5d4',max_age=1000)
     return resp
